362bb0efa3b4c8ffd61c62748a28ddfceb8d97b7
globaltec <globaltec.soft@gmail.com>
2014-03-11 10:06:49 -0300

    Alguns ajustes necessarios

---
+++ com.globaltec.fleetcontrol/src/main/java/com/globaltec/fleetcontrol/business/exception/NonexistentEntityException.java
@@ -0,0 +1,10 @@
+package com.globaltec.fleetcontrol.business.exception;
+
+public class NonexistentEntityException extends Exception {
+    public NonexistentEntityException(String message, Throwable cause) {
+        super(message, cause);
+    }
+    public NonexistentEntityException(String message) {
+        super(message);
+    }
+}
