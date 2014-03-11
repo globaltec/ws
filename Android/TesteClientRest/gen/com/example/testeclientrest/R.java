acf58a3b7e57d5a2aa8a62393002ab0deac8fa80
leandrodmartini <leandro.demartini@gmail.com>
2014-03-10 23:10:20 -0300

    adicionado logo e mudança de nome de packages do Bino

--- Android/TesteClientRest/gen/com/example/testeclientrest/R.java
+++ Android/TesteClientRest/gen/com/example/testeclientrest/R.java
@@ -980,7 +980,8 @@ containing a value of this type.
         public static final int abc_textfield_searchview_holo_light=0x7f020054;
         public static final int abc_textfield_searchview_right_holo_dark=0x7f020055;
         public static final int abc_textfield_searchview_right_holo_light=0x7f020056;
-        public static final int ic_launcher=0x7f020057;
+        public static final int ic_bino=0x7f020057;
+        public static final int ic_launcher=0x7f020058;
     }
     public static final class id {
         public static final int RLSaudacao=0x7f05003f;
---
+++ Android/TesteClientRest/src/com/globaltec/bino/MODEL/Usuario.java
@@ -0,0 +1,28 @@
+package com.globaltec.bino.MODEL;
+
+public class Usuario {
+
+	String nmLogin;
+	String senha;
+	
+	public Usuario() {
+		
+	}
+
+	public String getNmLogin() {
+		return nmLogin;
+	}
+
+	public void setNmLogin(String nmLogin) {
+		this.nmLogin = nmLogin;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	
+}
